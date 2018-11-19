2472c907832f860cc7a28ab58d844b376f48ec3e
kef <upload234@mail.ru>
2018-11-19 18:25:57 +0300

    Add parallel programmers

---
+++ arduinoparallel.py
@@ -0,0 +1,48 @@
+from time import sleep
+from serial import Serial
+
+
+class ArduinoParallel(object):
+
+    command_write_lpf_code = 'LPF'
+
+    def __init__(self, *args, **kwargs):
+        self._port = Serial(*args, **kwargs)
+
+        self._name = 'Parallel'
+
+    def __str__(self):
+        return f'{self._name} at {self._port.port}'
+
+    def write(self, command: str):
+        cmd = bytes(f'{command}\n', encoding='ascii')
+        print(f'{self._name} write: {cmd.strip()}')
+        return self._port.write(cmd)
+
+    def read_all(self):
+        answer = self._port.read_all()
+        print(f'{self._name} read_all: {answer}')
+        return answer
+
+    def query(self, question: str):
+        self.write(question)
+        # while not self._port.in_waiting:
+        #     pass
+        sleep(0.1)
+        return self.read_all()
+
+    def disconnect(self):
+        print(f'{self._name}: disconnect()')
+        if self._port.is_open:
+            self._port.close()
+
+    def set_lpf_code(self, code: int) -> bool:
+        print(f'{self._name}: set_lpf_code({code})')
+        comm = f'{self.command_write_lpf_code},{code}'
+        self.query(comm)
+        return True
+
+    @property
+    def name(self):
+        return f'{self._name} at {self._port.port}'
+
---
+++ arduinoparallelmock.py
@@ -0,0 +1,40 @@
+from arduino.arduinoparallel import ArduinoParallel
+
+
+class PortMock:
+
+    def __init__(self, port='COM5', baudrate=115200, parity='N', bytesize=8, stopbits=1, timeout=1):
+        self.port = port
+        self.baudrate = baudrate
+        self.parity = parity
+        self.bytesize = bytesize
+        self.stopbits = stopbits
+        self.timeout = timeout
+
+    def close(self):
+        return True
+
+    def write(self, command):
+        print(f'PortMock write: {command.strip()}')
+        return len(command)
+
+    def read_all(self):
+        answer = 'answer'
+        print(f'PortMock read_all: {answer}')
+        return answer
+
+    @property
+    def in_waiting(self):
+        return 10
+
+    @property
+    def is_open(self):
+        return True
+
+
+class ArduinoParallelMock(ArduinoParallel):
+
+    def __init__(self, *args, **kwargs):
+        self._port = PortMock()
+        self._name = 'Parallel mock'
+
