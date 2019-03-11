670caca6af9f54b7f6451c502c04cbc5447bee3f
igrekus <upload234@mail.ru>
2019-03-11 12:47:53 +0300

    WIP: add test for mkr_set_bit()

--- tests/test_jerome.py
+++ tests/test_jerome.py
@@ -153,3 +153,35 @@ def test_jerome_mkr_init(setup_jerome):
         IO23: PIN_OFF,
         IO24: PIN_OFF
     })
+
+
+def test_jerome_mkr_set_bit(setup_jerome):
+    j = setup_jerome
+
+    expect(j.mkr_set_bit(0, 1)).to_equal(('#WR,OK', '#WR,OK'))
+    expect(j.pin_states).to_equal({
+        IO1: PIN_OFF,
+        IO2: PIN_OFF,
+        IO3: PIN_OFF,
+        IO4: PIN_OFF,
+        IO5: PIN_OFF,
+        IO6: PIN_OFF,
+        IO7: PIN_OFF,
+        IO8: PIN_OFF,
+        IO9: PIN_ON,
+        IO10: PIN_OFF,
+        IO11: PIN_OFF,
+        IO12: PIN_OFF,
+        IO13: PIN_OFF,
+        IO14: PIN_OFF,
+        IO15: PIN_OFF,
+        IO16: PIN_OFF,
+        IO17: PIN_OFF,
+        IO18: PIN_OFF,
+        IO19: PIN_OFF,
+        IO20: PIN_OFF,
+        IO21: PIN_OFF,
+        IO22: PIN_OFF,
+        IO23: PIN_OFF,
+        IO24: PIN_OFF
+    })
